3301048038262c3b0e35119bb46fe118a8a865be
Jay Ciruolo <jciruolo@gmail.com>
2020-05-13 22:55:21 -0400

    remove vuelidate

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import App from './App.vue';
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
 import VueFormGenerator from 'vue-form-generator';
-import Vuelidate from 'vuelidate';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
@@ -11,8 +10,6 @@ import 'vue-form-generator/dist/vfg.css';
 Vue.use(BootstrapVue);
 Vue.use(BootstrapVueIcons);
 Vue.use(VueFormGenerator);
-
-Vue.use(Vuelidate);
 
 Vue.config.productionTip = false;
 
